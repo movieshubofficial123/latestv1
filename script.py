a58e7ad76c5f2f6350139b02209e49bf2a02214e
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-09-24 09:27:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,14 +2,7 @@ class script(object):
 
 
     START_MSG = """ <b>Hi {}
-
-You can call this as an Auto Filter Bot if you like :D
-
-Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
-
-So no need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+<i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
@@ -26,12 +19,12 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @AbinCr</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @AbinCr
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
